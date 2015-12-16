3abf9bddfa6fe792d665c0e4c0218815007e0bf7
Eros Fricker <e.fricker@hotmail.com>
2015-12-16 11:21:51 +0100

    Added camunda dependencies

---
+++ wirtschaftsinformatik/src/main/java/aardiifagtiiidii/WinfApplication.java
@@ -0,0 +1,9 @@
+package aardiifagtiiidii;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Winf Tasklist App")
+public class WinfApplication extends ServletProcessApplication {
+
+}
